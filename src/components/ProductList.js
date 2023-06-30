9d7108788f5521d99672d1f55a90c81f890eeb6c
Arpit Singh Yadav <arpitsinghyadav19@gmail.com>
2023-06-30 22:05:51 +0530

    Deployed website

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState,useContext } from 'react';
 import Product from './Product';
-// import MockData from '../data/products.json'
+import MockData from '../products.json'
 import axios from 'axios';
 import '../CSS/ProductList.css'
 import { Link,useNavigate } from 'react-router-dom'
@@ -8,15 +8,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faCartShopping} from '@fortawesome/free-solid-svg-icons'
 
 const ProductList = () => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState(MockData);
   const [search,setSearch]=useState('');
-  useEffect(()=>{
-    axios.get('http://localhost:3000/products').then(res=>{
-     setProducts(res.data)
-     console.log(res);
-  }).catch(err=> console.log(err))
-     },[])
-
   return (
     <div>
       <div className='productList'>
